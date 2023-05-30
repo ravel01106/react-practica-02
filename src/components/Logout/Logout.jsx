d59562181b27f617f570de7f7f6afa0218d3604e
ravel01106 <ravello2002@gmail.com>
2023-05-30 15:47:14 +0100

    feat(Logout): Now, we can see the name of the user in logout component

--- src/components/Logout/Logout.jsx
+++ src/components/Logout/Logout.jsx
@@ -1,16 +1,18 @@
 import PropTypes from 'prop-types'
 
-const Logout = ({handleNoIniciado}) => {
+const Logout = ({handleNoIniciado, userName}) => {
 
-  return (
-    <div>
-    <button className="btn btn-outline-light" type="button" onClick={handleNoIniciado}>Cerrar sesión</button>
-</div>
-  )
+    return (
+        <form className="container-fluid justify-content-start">
+            <div className='mx-3 text-white-50'>Bienvenido: <p className='d-inline text-white'>{userName}</p></div>
+            <button type='button' className='btn btn-warning text-success' onClick={handleNoIniciado}>Cerrar sesión</button>
+        </form>
+    )
 }
 
 Logout.propTypes = {
-    handleNoIniciado: PropTypes.func.isRequired
+    handleNoIniciado: PropTypes.func.isRequired,
+    userName : PropTypes.string.isRequired
 }
 
 export default Logout
